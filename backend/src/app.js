7750cb7108ae5d2da682745e697f75dee7e827c5
Aluno <e023aluno@outlook.com>
2026-08-12 20:34:41 -0300

    first commit

---
+++ backend/src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const produtoRoutes = require("./routes/produtoRoutes");
+
+    const app = express();
+    const PORTA = 3000;
+
+
+app.use(express.json());
+
+app.use("/api/produtos", produtoRoutes);
+
+
+app.listen(PORTA, ()=>{
+    console.log(`Servidor executando em http://localhost:${PORTA}`);
+});
---
+++ backend/src/routes/produtoRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const {
+
+    listarProdutos,
+    cadastrarProduto
+} = require("../controllers/produtocontroller");
+
+const router = express.Router();
+
+router.get("/", listarProdutos);
+router.post("/", cadastrarProduto);
+
+module.exports = router;
