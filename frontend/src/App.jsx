a875b5bd2b3e1843a33ec57cc05d302e9a092b7a
Aluno <e023aluno@outlook.com>
2026-08-12 22:07:04 -0300

    minha alteração

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import FormProduto from "./components/FormProduto"
+
+export default function App(    ){
+
+	return(
+		<>
+		<main className="container">
+				< FormProduto/>
+		</main>
+		</>
+	)
+}
